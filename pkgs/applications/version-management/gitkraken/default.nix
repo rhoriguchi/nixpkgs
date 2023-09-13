38ac9ce50e49b9af3574a476981c7b6d0a16d692
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-09-13 23:07:29 +0200

    gitkraken: 9.5.1 -> 9.8.1

--- pkgs/applications/version-management/gitkraken/default.nix
+++ pkgs/applications/version-management/gitkraken/default.nix
@@ -10,24 +10,24 @@ with lib;
 
 let
   pname = "gitkraken";
-  version = "9.5.1";
+  version = "9.8.1";
 
   throwSystem = throw "Unsupported system: ${stdenv.hostPlatform.system}";
 
   srcs = {
     x86_64-linux = fetchzip {
       url = "https://release.axocdn.com/linux/GitKraken-v${version}.tar.gz";
-      sha256 = "sha256-irKs0yvz2TrKvF34DMOBdmJvH+Lox/ZVbPSaHAl6Vyo=";
+      sha256 = "sha256-fPJ4GZ22qJz/29TFoa84kpHFJEoqtEmPdtPEmGRxoB8=";
     };
 
     x86_64-darwin = fetchzip {
       url = "https://release.axocdn.com/darwin/GitKraken-v${version}.zip";
-      sha256 = "sha256-3g49FBbolEhBgSPanLnrWhfxHR5jg4C1p+70rIrQ2GM=";
+      sha256 = "sha256-Ed9DN6io/dFQ8fPgTCySYptYaaPxsvUzEi0tNKD6jZs=";
     };
 
     aarch64-darwin = fetchzip {
       url = "https://release.axocdn.com/darwin-arm64/GitKraken-v${version}.zip";
-      sha256 = "sha256-8ateh2LswWMOboPASWcYTy6OfK30h7wABIgoZXJ7GTM=";
+      sha256 = "sha256-ZZHMl6+9z7eE/bNrD5YJ7iyNXQOLBx9ntuhmsqryTGI=";
     };
   };
 
