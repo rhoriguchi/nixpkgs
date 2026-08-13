4b57d530eca15820d9956d11732b8edad771dc34
Harinn <prinn.dev@pm.me>
2026-08-13 22:39:31 +0700

    python3Packages.dbfread: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/dbfread/default.nix
+++ pkgs/development/python-modules/dbfread/default.nix
@@ -2,17 +2,20 @@
   lib,
   fetchPypi,
   buildPythonPackage,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "dbfread";
   version = "2.0.7";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "07c8a9af06ffad3f6f03e8fe91ad7d2733e31a26d2b72c4dd4cfbae07ee3b73d";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     description = "Read DBF Files with Python";
