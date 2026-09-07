086783858f4ef56f9daed56a5797311d940884f3
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:24 +0700

    python3Packages.django-paintstore: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/django-paintstore/default.nix
+++ pkgs/development/python-modules/django-paintstore/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "django-paintstore";
   version = "0.2";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "12wxgwv1qbkfq7w5i7bm7aidv655c2sxp0ym73qf8606dxbjcwwg";
   };
+
+  build-system = [ setuptools ];
 
   doCheck = false;
 
