52f968e679efe317a0d1b813d2a038ed1a75c073
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:24 +0700

    python3Packages.django-pglocks: modernize

--- pkgs/development/python-modules/django-pglocks/default.nix
+++ pkgs/development/python-modules/django-pglocks/default.nix
@@ -7,10 +7,12 @@
   six,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "django-pglocks";
   version = "1.0.4";
   pyproject = true;
+
+  __structuredAttrs = true;
 
   meta = {
     description = "PostgreSQL locking context managers and functions for Django";
@@ -19,8 +21,8 @@ buildPythonPackage rec {
   };
 
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "3c47c66fbfbd268ef46269673a0516a039539b0972b8ed2ec9cfee44c4b65523";
+    inherit (finalAttrs) pname version;
+    hash = "sha256-PEfGb7+9Jo70YmlnOgUWoDlTmwlyuO0uyc/uRMS2VSM=";
   };
 
   build-system = [ setuptools ];
@@ -33,4 +35,6 @@ buildPythonPackage rec {
 
   # tests need a postgres database
   doCheck = false;
-}
+
+  pythonImportsCheck = [ "django_pglocks" ];
+})
